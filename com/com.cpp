9030ac369524356012f65362c6d73afa464bd6c7
asdfvar <asdfvar@gmail.com>
2017-06-16 22:02:30 -0700

    more changes to communication library

--- com/com.cpp
+++ com/com.cpp
@@ -1,61 +1,66 @@
 #include "com.h"
-#include <mpi.h>
 
 namespace com {
 
-   int send( int* buf,
-             int  count,
-             int  dest_id )
+   int Com::send( int* buf,
+                  int  count,
+                  int  dest_id )
    {
+      MPI_Request request;
       // perform a non-blocking send
-      return MPI_Isend ( buf, count, MPI_INT, dest_id, MPI_TAG_ANY, MPI_COMM_WORLD );
+      return MPI_Isend ( buf, count, MPI_INT, dest_id, MPI_ANY_TAG, MPI_COMM_WORLD, &request );
    }
 
-   int send( float* buf,
-             int    count,
-             int    dest_id )
+   int Com::send( float* buf,
+                  int    count,
+                  int    dest_id )
    {
+      MPI_Request request;
       // perform a non-blocking send
-      return MPI_Isend ( buf, count, MPI_FLOAT, dest_id, MPI_TAG_ANY, MPI_COMM_WORLD );
+      return MPI_Isend ( buf, count, MPI_FLOAT, dest_id, MPI_ANY_TAG, MPI_COMM_WORLD, &request );
    }
 
-   int send( double* buf,
-             int     count,
-             int     dest_id )
+   int Com::send( double* buf,
+                  int     count,
+                  int     dest_id )
    {
+      MPI_Request request;
       // perform a non-blocking send
-      return MPI_Isend ( buf, count, MPI_DOUBLE, dest_id, MPI_TAG_ANY, MPI_COMM_WORLD );
+      return MPI_Isend ( buf, count, MPI_DOUBLE, dest_id, MPI_ANY_TAG, MPI_COMM_WORLD, &request );
    }
 
-   int recv( int* buf,
-             int  count,
-             int  src_id )
+   int Com::recv( int* buf,
+                  int  count,
+                  int  src_id )
    {
-      MPI_Status status;
+      MPI_Comm    status;
+      MPI_Request request;
 
       // perform a non-blocking receive
-      return MPI_Irecv ( buf, count, MPI_INT, src_id, MPI_TAG_ANY, &status );
+      return MPI_Irecv ( buf, count, MPI_INT, src_id, MPI_ANY_TAG, status, &request );
    }
 
-   int recv( float* buf,
-             int    count,
-             int    src_id )
+   int Com::recv( float* buf,
+                  int    count,
+                  int    src_id )
    {
-      MPI_Status status;
+      MPI_Comm    status;
+      MPI_Request request;
 
       // perform a non-blocking receive
-      return MPI_Irecv ( buf, count, MPI_FLOAT, src_id, MPI_TAG_ANY, &status );
+      return MPI_Irecv ( buf, count, MPI_FLOAT, src_id, MPI_ANY_TAG, status );
    }
 
-   int recv( double*     buf,
-             int         count,
-             int         src_id,
-             MPI_Status* status )
+   int Com::recv( double*     buf,
+                  int         count,
+                  int         src_id,
+                  MPI_Status* status )
    {
-      MPI_Status status;
+      MPI_Comm    status;
+      MPI_Request request;
 
       // perform a non-blocking receive
-      return MPI_Irecv ( buf, count, MPI_DOUBLE, src_id, MPI_TAG_ANY, status );
+      return MPI_Irecv ( buf, count, MPI_DOUBLE, src_id, MPI_ANY_TAG, status );
    }
 
 }
--- com/com.h
+++ com/com.h
@@ -1,36 +1,45 @@
 #ifndef COM_H
 #define COM_H
 
+#include <mpi.h>
+
 namespace com {
 
-int send( int* buf,
-          int  count, 
-          int  dest_id );
+   class Com {
 
-int send( float* buf,
-          int    count, 
-          int    dest_id );
+    private:
 
-int send( double* buf,
-          int     count, 
-          int     dest_id );
+      int* volume[1024];
 
-int recv( int* buf,
-          int  count,
-          int  dest_id );
+      int send( int* buf,
+                int  count, 
+                int  dest_id );
 
-int recv( float* buf,
-          int    count,
-          int    dest_id );
+      int send( float* buf,
+                int    count, 
+                int    dest_id );
 
-int recv( double* buf,
-          int     count,
-          int     dest_id );
+      int send( double* buf,
+                int     count, 
+                int     dest_id );
 
-int wait( int* buf,
-          int  count,
-          int  src_id,
-          MPI_Status* status );
+      int recv( int* buf,
+                int  count,
+                int  dest_id );
+
+      int recv( float* buf,
+                int    count,
+                int    dest_id );
+
+      int recv( double* buf,
+                int     count,
+                int     dest_id );
+
+      int wait( int* buf,
+                int  count,
+                int  src_id,
+                MPI_Status* status );
+   };
 }
 
 #endif
