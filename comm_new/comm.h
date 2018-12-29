acf6a3b281a05465e298151595e18bdeee6a2c0f
asdfvar <asdfvar@gmail.com>
2018-12-28 16:29:00 -0800

    updates to comm class. added 2D and 3D derived classes

--- comm_new/comm.h
+++ comm_new/comm.h
@@ -6,7 +6,7 @@
 
 #define MAX_TAGS   200
 #define MAX_STAGES 20
-#define MAX_INTERCOMMS MAX_STAGES * (MAX_STAGES - 1)
+#define MAX_INTERCOMMS MAX_STAGES
 #define INVALID_TAG -1
 
 namespace comm {
@@ -16,28 +16,107 @@ struct CONFIG {
    int numStages;
 };
 
+/*
+** class name: COMM
+*/
 class COMM {
 
    public:
-      COMM (int* argc, char*** argv, CONFIG& config);
+      COMM (int* argc, char*** argv, CONFIG& config, const unsigned int numStages, const unsigned int thisStageNum);
+
      ~COMM (void);
 
-      template<typename type> bool send_to_stage (type* data, int size, int stage, int recvStageRank, int tag);
+      template<typename type> bool send_to_stage (type* data, int size, int recvStage, int recvStageRank, int tag);
+      bool wait_for_send_to_stage (unsigned int recvStage, unsigned int nextStageRank, int tag);
       template<typename type> bool send_to_stage (type* data, int size, int stage, int tag) {
          return send_to_stage (data, size, stage, 0, tag);
       }
 
-   private:
-      MPI_Comm interComms[MAX_INTERCOMMS];
+      template <typename type> bool receive_from_stage (type* data, int dataSize, unsigned int sendStage, unsigned int stageRank, int tag);
+      bool wait_for_receive_from_stage (unsigned int sendStage, unsigned int sendStageRank, int tag);
 
-      int sendTags[MAX_TAGS];
-      int recvTags[MAX_TAGS];
+   protected:
 
-      std::vector< std::vector< std::vector<MPI_Request*> > > sendRequests;
-      std::vector< std::vector< std::vector<MPI_Request*> > > recvRequests;
+      // number of processes for each stage
+      unsigned int* numStageProcs;
+
+      MPI_Group stageGroups[MAX_STAGES];
+      MPI_Comm  stageComms[MAX_STAGES];
+      MPI_Comm  interComms[MAX_INTERCOMMS];
+
+      // accounting
+      int tagsToStage[MAX_STAGES][MAX_TAGS];
+      int tagsFromStage[MAX_STAGES][MAX_TAGS];
+      unsigned int thisStageNum;
+      int sendToStageHandles[MAX_STAGES];
+      int numSendToStageHandles[MAX_STAGES];
+      int numReceiveFromStageHandles[MAX_STAGES];
+
+      // request handles of dimension [stage][tag_index][rank]
+      std::vector< std::vector< std::vector<MPI_Request*> > > sendToStageRequests;
+      std::vector< std::vector< std::vector<MPI_Request*> > > receiveFromStageRequests;
 
 };
 
-}
+/*
+** class name: COMM2D from COMM
+*/
+class COMM2D : public COMM {
 
+   public:
+
+      COMM2D (int argc, char* argv[], const CONFIG& config, const unsigned int numStages,
+              const unsigned int thisStageNum, const unsigned int tiles[2]);
+
+      ~COMM2D (void);
+
+   protected:
+
+      int                        tagsDim0and1[MAX_TAGS];
+      int                        numCommSendDim0and1Handles;
+      int                        waitingStage[MAX_TAGS];
+      int                        numTilesDim0;
+      int                        numTilesDim1;
+      std::vector <MPI_Request*> localSendRequestsDim0and1;
+      std::vector <MPI_Request*> localReceiveRequestsDim0and1;
+
+   private:
+
+      MPI_Comm dimensions0and1Comm;
+      int      dimensions0and1Rank;
+
+};
+
+/*
+** class name: COMM3D from COMM
+*/
+class COMM3D : public COMM2D {
+
+   public:
+
+      COMM3D (int argc, char* argv[], const CONFIG& config, const unsigned int numStages,
+              const unsigned int thisStageNum, const unsigned int tiles[3]);
+
+      ~COMM3D (void);
+
+      template <typename type> bool send_to_next_dim2 (type* data, int src_size, int tag);
+      template <typename type> bool receive_from_previous_dim2 (type* data, int src_size, int tag);
+
+   protected:
+
+      int                        tagsDim2[MAX_TAGS];
+      int                        numCommSendDim2Handles;
+      int                        numTilesDim2;
+      std::vector <MPI_Request*> localSendRequestsDim2;
+      std::vector <MPI_Request*> localReceiveRequestsDim2;
+      bool                       sendingToDim2[MAX_TAGS];
+      bool                       receivingFromDim2[MAX_TAGS];
+
+   private:
+
+      MPI_Comm dimension2Comm;
+      int      dimension2Rank;
+};
+
+} // namespace comm
 #endif
