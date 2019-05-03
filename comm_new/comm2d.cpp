48fc1758a9d2cd939559ff9166f28fcaf7dfb013
asdfvar <asdfvar@gmail.com>
2019-05-02 22:42:51 -0700

    removed original, legacy code that was initially implemented for 2D communication intitialization

--- comm_new/comm2d.cpp
+++ comm_new/comm2d.cpp
@@ -13,44 +13,6 @@ COMM2D::COMM2D (
 {
    numTilesDim0 = tiles[0];
    numTilesDim1 = tiles[1];
-
-#if 0
-   // initialize the communication handles to null
-   dimensions0and1Comm = MPI_COMM_NULL;
-
-   // initialize the number of communication handles to zero
-   numCommSendDim0and1Handles = 0;
-
-   // initialize status flags
-
-   // get the rank of this process within this stage
-   int localRank;
-   MPI_Comm_rank (stageComms[thisStageNum], &localRank);
-
-   int numTilesDim0and1 = tiles[0] * tiles[1];
-
-   // establish the tiles 0 & 1 group and its corresponding intra-communicator from the group of all processes within this stage. Partitions within each dim2 will be able to communicate with each other with this communicator handle but not across dim2
-   const int tiles0and1dim2 = localRank / numTilesDim0and1;
-   const int tiles0a1Rankings = tiles0and1dim2 * numTilesDim0and1;
-   int tiles0and1GroupRankings[numTilesDim0and1];
-   for (int rank = tiles0a1Rankings, ind = 0; ind < numTilesDim0and1; ind++, rank++)
-   {
-      tiles0and1GroupRankings[ind] = rank;
-   }
-
-   MPI_Group tiles0and1Group;
-   MPI_Group_incl (stageGroups[thisStageNum], numTilesDim0and1, tiles0and1GroupRankings, &tiles0and1Group);
-
-   // create communication handle for dimensions 0 and 1
-   MPI_Comm_create (stageComms[thisStageNum], tiles0and1Group, &dimensions0and1Comm);
-
-   // get the rank for dimensions 0 and 1 communicator
-   MPI_Comm_rank (dimensions0and1Comm, &dimensions0and1Rank);
-
-   // free no longer needed group handles
-   MPI_Group_free (&tiles0and1Group);
-#endif
-
 }
 
 COMM2D::~COMM2D (void)
