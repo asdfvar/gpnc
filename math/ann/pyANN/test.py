88b65000ffddf1b8859fade6ab006110c597a0ed
asdfvar <asdfvar@gmail.com>
2020-10-04 23:24:27 -0700

    updates to ANN documentation and Python files; updated gitignore

--- math/ann/pyANN/test.py
+++ math/ann/pyANN/test.py
@@ -4,42 +4,65 @@ import ann
 import numpy as np
 from matplotlib import pyplot as plt
 import random
+import time
 
-NN = ann.ANN ([2, 3, 2, 2], 0.99)
-
-offset = np.array ([0.0, 4.0])
+NN = ann.ANN ([2, 3, 3, 1], 0.9)
 
 Input = []
-Input.append (np.array ([1.0, 1.0]) + offset)
-Input.append (np.array ([1.1, 0.9]) + offset)
-Input.append (np.array ([0.9, 0.7]) + offset)
-Input.append (np.array ([0.8, 0.9]) + offset)
-
-Input.append (np.array ([-1.0, -1.0]) + offset)
-Input.append (np.array ([-1.1, -0.9]) + offset)
-Input.append (np.array ([-0.9, -0.7]) + offset)
-Input.append (np.array ([-0.8, -0.9]) + offset)
-
 Output = []
-Output.append (np.array ([1.0, 0.0]))
-Output.append (np.array ([1.0, 0.0]))
-Output.append (np.array ([1.0, 0.0]))
-Output.append (np.array ([1.0, 0.0]))
 
-Output.append (np.array ([0.0, 1.0]))
-Output.append (np.array ([0.0, 1.0]))
-Output.append (np.array ([0.0, 1.0]))
-Output.append (np.array ([0.0, 1.0]))
+for ind in range (6):
+   Input.append (np.random.normal ([-1.0, 0.0], 0.2))
+   Output.append (np.array ([1.0]))
 
-N = 500000
+for ind in range (6):
+   Input.append (np.random.normal ([1.0, 0.0], 0.2))
+   Output.append (np.array ([1.0]))
+
+for ind in range (6):
+   Input.append (np.random.normal ([1.0, 1.0], 0.2))
+   Output.append (np.array ([0.0]))
+
+for ind in range (6):
+   Input.append (np.random.normal ([0.0, 0.0], 0.2))
+   Output.append (np.array ([0.0]))
+
+# Train
+N = 5000000
 error_list = np.zeros (N)
+start = time.time ()
 for k in range (N):
-   randind = random.randint (0, 7)
+   randind = random.randint (0, len (Input) - 1)
    Error = NN.back (Input[randind], Output[randind], 0.01)
    error_list[k] = Error
+end = time.time ()
+print ("training time: " + str (end - start))
+
+print ("weights:")
+for weights in NN.weights:
+   print (weights)
+   print ()
 
 plt.plot (error_list)
+plt.title ("Error")
 plt.show ()
 
-print (NN.forward (np.array ([2, 2])))
-print (NN.forward (np.array ([-1, -1])))
+xgrid = np.linspace (-2.0, 2.0, 100)
+ygrid = np.linspace (-2.0, 2.0, 100)
+
+start = time.time ()
+for y in ygrid:
+   for x in xgrid:
+      value = NN.forward (np.array ([x, y]))[0]
+      if value >= 0.5: Color = 'darksalmon'
+      if value <  0.5: Color = 'aqua'
+      plt.plot (x, y, '.', color = Color)
+
+for ind in range (len (Input)):
+   if Output[ind][0] >= 0.5: Color = 'r'
+   if Output[ind][0] <  0.5: Color = 'b'
+   plt.plot (Input[ind][0], Input[ind][1], '.', color = Color)
+end = time.time ()
+print ("plotting time: " + str (end - start))
+
+plt.show ()
