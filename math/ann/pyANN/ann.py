be8df22af6bbb30261e08363bd0b4774a6192447
asdfvar <asdfvar@gmail.com>
2020-11-04 20:58:21 -0800

    changed everything to a tensor-like type

--- math/ann/pyANN/ann.py
+++ math/ann/pyANN/ann.py
@@ -1,71 +1,120 @@
 #!/usr/bin/python3
 
+# inputs are tensors which are defined as Python lists with dimensions like so:
+# [batch, channel, 2-D numpy array]
+#
+# in some cases, they are expressed as [layer, batch, channel, 2-D numpy array]
+
 import numpy as np
 
 class ANN:
-   def __init__(self, layers, actFunc, beta = 0.9):
+   def __init__(self, layers, numChannels, actFunc, beta = 0.9):
       self.N = len (layers)
-      self.weights = [None] * (self.N - 1)
-      self.bias    = [None] * (self.N - 1)
-      self.psi     = [None] * (self.N)
-      self.y       = [None] * (self.N)
+      self.numChannels = numChannels
 
       actFuncs = {"sigmoid": self.sigmoid (beta),
                   "unit":    self.unit () }
 
-      self.g       = actFuncs[actFunc]
+      self.g = actFuncs[actFunc]
 
-      for ind in range (len (self.weights)):
-         # weights
-         self.weights[ind] = np.random.rand (layers[ind + 1], layers[ind])
-         # bias weights
-         self.bias[ind] = np.random.rand (layers[ind + 1])
+      # the following are expressed as [layer][channel][numpy array]
+      self.weights = []
+      self.bias    = []
+      for layer in range (len (layers)):
+         self.weights.append ([None] * numChannels[layer])
+         self.bias.append    ([None] * numChannels[layer])
 
+      for layer in range (self.N - 1):
+         for channel in range (len (self.weights[layer])):
+            # weights
+            self.weights[layer][channel] = np.random.rand (layers[layer + 1], layers[layer])
+            # bias weights
+            self.bias[layer][channel]    = np.random.rand (layers[layer + 1])
+
+   # "Input" is in the form of [batch][channel][numpy array]
    def forward (self, Input):
-      y = Input
-      for ind in range (self.N - 1):
-         self.y[ind] = y;
-         y  = np.matmul (self.weights[ind], y)
-         y += self.bias[ind]
-         self.psi[ind] = y
-         y  = self.g.activate (y)
-      return y
 
+      # the following are expressed as [layer][batch][channel][numpy array]
+      self.psi   = []
+      self.y     = []
+      self.delta = []
+      for layer in range (self.N):
+         larray = []
+         for batch in range (len (Input)):
+            lbatch = []
+            for channel in range (self.numChannels[layer]):
+               lbatch.append ([None])
+            larray.append    (lbatch)
+         self.psi.append     (larray)
+         self.y.append       (larray)
+         self.delta.append   (larray)
+
+      self.y[0] = Input
+      for layer in range (self.N - 1):
+         for batch in range (len (self.y[layer])):
+            for channel in range (len (self.y[layer][batch])):
+               self.psi[layer][batch][channel] = np.matmul (self.weights[layer][channel], self.y[layer][batch][channel]) + self.bias[layer][channel]
+               self.y[layer + 1][batch][channel] = self.g.activate (self.psi[layer][batch][channel])
+      return self.y[self.N - 1]
+
+   # "Input" is in the form of [batch, channel, 2-D numpy array]
+   # "Output" is in the form of [batch, channel, 2-D numpy array]
+   # "stepsize" is a positive real number specifying the step size used in gradient descent
    def back (self, Input, Output, stepsize):
       z = self.forward (Input)
-      diff = z - Output
-      gamma = diff * self.g.dactivate (self.psi[self.N - 2])
+      # the following are expressed as [batch, channel, numpy array]
+      diff  = []
+      gamma = []
+      dEdw = []
+      dEdu = []
+      for batch in range (len (z)):
+         diff.append  ([None])
+         gamma.append ([None])
+         dEdw.append  ([None])
 
-      for ind in np.arange (self.N - 2, -1, -1):
-         if (ind == self.N - 2):
-            dEdw = np.outer (gamma, self.y[ind])
-            self.weights[ind] -= stepsize * dEdw
-            dEdu = gamma
-            self.bias[ind]    -= stepsize * dEdu
+      for batch in range (len (self.y[self.N - 2])):
+         for channel in range (len (self.y[self.N - 2][batch])):
+            diff[batch][channel] = z[batch][channel] - Output[batch][channel]
+            gamma[batch][channel] = diff[batch][channel] * self.g.dactivate (self.psi[self.N - 2][batch][channel])
 
-            # update delta for the next layer
-            delta = np.matmul (gamma, self.weights[ind])
-         else:
+      for layer in np.arange (self.N - 2, -1, -1):
+         for batch in range (len (self.y[layer])):
+            for channel in range (len (self.y[layer][batch])):
+               # last layer
+               if (layer == self.N - 2):
+                  dEdw = np.outer (gamma[batch][channel], self.y[layer][batch][channel])
+                  self.weights[layer][channel] -= stepsize * dEdw
+                  dEdu = gamma[batch][channel]
+                  self.bias[layer][channel]    -= stepsize * dEdu
 
-            # use the previously calculated delta to update the weights
-            LHS  = np.tile (delta, (len (self.y[ind]), 1)).transpose ()
-            gp   = self.g.dactivate (self.psi[ind])
-            RHS  = np.outer (gp, self.y[ind])
-            dEdw = LHS * RHS
+                  # update delta for the next layer
+                  self.delta[layer][batch][channel] = np.matmul (gamma[batch][channel], self.weights[layer][channel])
 
-            self.weights[ind] -= stepsize * dEdw
-            dEdu = delta * gp
-            self.bias[ind]    -= stepsize * dEdu
+               # all other interior layers
+               else:
+                  # use the previously calculated delta to update the weights
+                  LHS  = np.tile (self.delta[layer][batch][channel], (len (self.y[layer]), 1)).transpose ()
+                  gp   = self.g.dactivate (self.psi[layer][batch][channel])
+                  RHS  = np.outer (gp, self.y[layer])
+                  dEdw = LHS * RHS
 
-            if ind > 0:
-               # update delta for the next layer
-               Gp    = np.diag (gp)
-               Gpw   = np.matmul (Gp, self.weights[ind])
-               delta = np.matmul (delta, Gpw)
+                  self.weights[layer][channel] -= stepsize * dEdw
+                  dEdu          = self.delta[layer][batch][channel] * gp
+                  self.bias[layer]             -= stepsize * dEdu
 
+                  if layer > 0:
+                     # update delta for the next layer
+                     Gp    = np.diag (gp)
+                     Gpw   = np.matmul (Gp, self.weights[layer][channel])
+                     self.delta[layer][batch][channel] = np.matmul (self.delta[layer][batch][channel], Gpw)
+
+      Error = 0.0
       z = self.forward (Input)
-      diff = z - Output
-      return 0.5 * np.sum (diff * diff)
+      for batch in range (len (self.y[self.N - 2])):
+         for channel in range (len (self.y[self.N - 2][batch])):
+            diff[batch][channel] = z[batch][channel] - Output[batch][channel]
+            Error += 0.5 * np.sum (diff[batch][channel] * diff[batch][channel])
+      return Error
 
    class activation:
       def activate  (self, x): pass
--- math/ann/pyANN/test/test.py
+++ math/ann/pyANN/test/test.py
@@ -12,42 +12,43 @@ import random
 import time
 import progressbar
 
-NN = ann.ANN ([2, 3, 3, 1], "sigmoid", 0.9)
+NN = ann.ANN ([2, 3, 3, 1], [1, 1, 1, 1], "sigmoid", 0.9)
 
-Input = []
+Input  = []
 Output = []
 
 for ind in range (6):
-   Input.append (np.random.normal ([-1.0, -1.0], 0.2))
-   Output.append (np.array ([1.0]))
+   Input.append  ([np.random.normal ([-1.0, -1.0], 0.2)])
+   Output.append ([np.array ([1.0])])
 
 for ind in range (6):
-   Input.append (np.random.normal ([1.0, -1.0], 0.2))
-   Output.append (np.array ([1.0]))
+   Input.append  ([np.random.normal ([1.0, -1.0], 0.2)])
+   Output.append ([np.array ([1.0])])
 
 for ind in range (6):
-   Input.append (np.random.normal ([0.0, 1.0], 0.2))
-   Output.append (np.array ([1.0]))
+   Input.append  ([np.random.normal ([0.0, 1.0], 0.2)])
+   Output.append ([np.array ([1.0])])
 
 for ind in range (6):
-   Input.append (np.random.normal ([-1.0, 1.0], 0.2))
-   Output.append (np.array ([0.0]))
+   Input.append  ([np.random.normal ([-1.0, 1.0], 0.2)])
+   Output.append ([np.array ([0.0])])
 
 for ind in range (6):
-   Input.append (np.random.normal ([1.0, 1.0], 0.2))
-   Output.append (np.array ([0.0]))
+   Input.append  ([np.random.normal ([1.0, 1.0], 0.2)])
+   Output.append ([np.array ([0.0])])
 
 for ind in range (6):
-   Input.append (np.random.normal ([0.0, -1.0], 0.2))
-   Output.append (np.array ([0.0]))
+   Input.append  ([np.random.normal ([0.0, -1.0], 0.2)])
+   Output.append ([np.array ([0.0])])
 
 # Train
 N = 10000000
 error_list = np.zeros (N)
 start = time.time ()
+#for k in range (N):
 for k in progressbar.progressbar (range (N)):
    randind = random.randint (0, len (Input) - 1)
-   Error = NN.back (Input[randind], Output[randind], 0.01)
+   Error = NN.back ([Input[randind]], [Output[randind]], 0.01)
    error_list[k] = Error
 end = time.time ()
 print ("training time: " + str (end - start))
@@ -68,15 +69,15 @@ fig2 = plt.figure (2)
 start = time.time ()
 for y in progressbar.progressbar (ygrid):
    for x in xgrid:
-      value = NN.forward (np.array ([x, y]))[0]
-      if value >= 0.5: Color = 'darksalmon'
-      if value <  0.5: Color = 'aqua'
+      value = NN.forward ([[np.array ([x, y])]])[0]
+      if value[0][0] >= 0.5: Color = 'darksalmon'
+      if value[0][0] <  0.5: Color = 'aqua'
       plt.plot (x, y, '.', color = Color)
 
 for ind in range (len (Input)):
    if Output[ind][0] >= 0.5: Color = 'r'
    if Output[ind][0] <  0.5: Color = 'b'
-   plt.plot (Input[ind][0], Input[ind][1], '.', color = Color)
+   plt.plot (Input[ind][0][0], Input[ind][0][1], '.', color = Color)
 end = time.time ()
 print ("plotting time: " + str (end - start))
 
