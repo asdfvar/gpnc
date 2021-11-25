1668de1d279e747201ae58d38efe8b5cca98dd4b
asdfvar <asdfvar@gmail.com>
2021-11-24 22:28:22 -0800

    removed old CAS; moved new CAS to old filename

--- cas/cas.py
+++ cas/cas.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import copy
 
 class Variable:
    def __init__ (self, term, coef = 1, exp = 1):
@@ -13,30 +12,28 @@ class Variable:
    def __add__ (self, other):
       ret = None
       if isinstance (self.term, type (other.term)):
-         if isinstance (self.term, int) or isinstance (self.term, float) or isinstance (self.term, complex):
+         if isinstance (self.term, int) or \
+            isinstance (self.term, float) or \
+            isinstance (self.term, complex):
             ret = Variable (other.term + self.term)
          elif self.term == other.term and self.exp == other.exp:
             ret = Variable (self.term, self.coef + other.coef)
       else:
-         ret = Addition (self, other)
+         ret = OperationBi (self, other, '+')
       return ret
 
    def __mul__ (self, other):
       ret = None
       if isinstance (self.term, type (other.term)):
-         if isinstance (self.term, int) or isinstance (self.term, float) or isinstance (self.term, complex):
+         if isinstance (self.term, int) or \
+            isinstance (self.term, float) or \
+            isinstance (self.term, complex):
             ret = Variable (other.term * self.term)
-         elif self.term == other.term and self.exp == other.exp:
-            ret = Variable (self.term, self.coef * other.coef)
+         elif self.term == other.term:
+            ret = Variable (self.term, self.coef * other.coef, self.exp + other.exp)
+      else:
+         ret = OperationBi (self, other, '*')
       return ret
-
-   def is_addative_id (self):
-      if self.term == 0: return True
-      return False
-
-   def is_multiplicative_id (self):
-      if self.term == 1: return True
-      return False
 
    def __str__ (self):
       components = ""
@@ -47,104 +44,42 @@ class Variable:
          components += "^" + str (self.exp)
       return components
 
-class Addition:
+class OperationBi:
 
-   def __init__ (self, term1, term2):
-      self.terms = list()
-      if isinstance (term1, Addition):
-         self.terms += term1.terms
-      else:
-         self.terms.append (term1)
-      if isinstance (term2, Addition):
-         self.terms += term2.terms
-      else:
-         self.terms.append (term2)
+   def __init__ (self, term1, term2, Type):
+      self.term1 = term1
+      self.term2 = term2
+      self.Type  = Type
 
    def evaluate (self):
-      inda = 0
-      while inda < len (self.terms):
-         indb = inda + 1
-         if isinstance (self.terms[inda], Addition):
-            self.terms[inda].evaluate ()
-         elif isinstance (self.terms[inda], Multiplication):
-            self.terms[inda].evaluate ()
-         else:
-            while indb < len (self.terms):
-               while isinstance (self.terms[inda], Variable) and \
-                     isinstance (self.terms[indb], Variable) and \
-                     type (self.terms[inda].term) == type (self.terms[indb].term):
-                  self.terms[inda] += self.terms[indb]
-                  self.terms.pop (indb)
-                  if indb >= len (self.terms): break
-               indb += 1
-         inda += 1
+
+      if isinstance (self.term1, OperationBi):
+         self.term1 = self.term1.evaluate ()
+      if isinstance (self.term2, OperationBi):
+         self.term2 = self.term2.evaluate ()
+
+      if isinstance (self.term1, Variable) and isinstance (self.term2, Variable):
+         if self.Type == '+':
+            return self.term1 + self.term2
+         if self.Type == '*':
+            return self.term1 * self.term2
+
+      return self
 
    def __str__ (self):
-      components = None
-      for term in self.terms:
-         if components is None:
-            components = str (term)
-         else:
-            components += " + " + str (term)
-
-      if components is None:
-         return ""
-      else:
-         return components
-
-class Multiplication:
-
-   def __init__ (self, term1, term2):
-      self.terms = list()
-      if isinstance (term1, Multiplication):
-         self.terms += term1.terms
-      else:
-         self.terms.append (term1)
-      if isinstance (term2, Multiplication):
-         self.terms += term2.terms
-      else:
-         self.terms.append (term2)
-
-   def evaluate (self):
-      inda = 0
-      while inda < len (self.terms):
-         indb = inda + 1
-         if isinstance (self.terms[inda], Addition):
-            self.terms[inda].evaluate ()
-         elif isinstance (self.terms[inda], Multiplication):
-            self.terms[inda].evaluate ()
-         else:
-            while indb < len (self.terms):
-               while isinstance (self.terms[inda], Variable) and \
-                     isinstance (self.terms[indb], Variable) and \
-                     type (self.terms[inda].term) == type (self.terms[indb].term):
-                  self.terms[inda] *= self.terms[indb]
-                  self.terms.pop (indb)
-                  if indb >= len (self.terms): break
-               indb += 1
-         inda += 1
-
-   def __str__ (self):
-      components = None
-      for term in self.terms:
-         if components is None:
-            components = str (term)
-         else:
-            components += " * " + str (term)
-
-      if components is None:
-         return ""
-      else:
-         return components
+      if self.Type == '+':
+         return "(" + str (self.term1) + " + " + str (self.term2) + ")"
+      if self.Type == '*':
+         return "(" + str (self.term1) + " * " + str (self.term2) + ")"
+      return None
 
 if __name__ == "__main__":
-   op1 = Addition (Variable (7), Variable (5))
-   op2 = Addition (Variable (11), Variable (100))
-   op  = Addition (op1, op2)
-   op  = Addition (op, Variable ('a'))
-   op  = Addition (op, Variable (7))
-   op  = Addition (op, Multiplication (Variable (4), Variable (5)))
+   op = OperationBi (Variable (7), Variable (5), '+')
+   op = OperationBi (op, Variable (100), '+')
+   op  = OperationBi (op, Variable ('a'), '+')
+   op  = OperationBi (op, OperationBi (Variable (4), Variable (5), '*'), '+')
+   op  = OperationBi (op, Variable (7), '+')
 
    print ("binary operand consists of " + str (op))
-   op.evaluate ()
-   print ("Which evaluates to " + str (op))
+   result = op.evaluate ()
+   print ("Which evaluates to " + str (result))
