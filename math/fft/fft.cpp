5ac4f6f146b59865a100a56834522029b894b82a
asdfvar <asdfvar@gmail.com>
2022-01-23 18:12:41 -0800

    removed diagnostics

--- math/fft/fft.cpp
+++ math/fft/fft.cpp
@@ -24,7 +24,6 @@ namespace fft {
          y[i] = complex<float> (0.0f, 0.0f);
          for (int j = 0, jcount = 0; jcount < N; j += stride, jcount++) {
             y[i] += w[((i*j)/stride) % (N*stride)] * x[j];
-std::cout << "i" << i << ",j" << j << ": w" << w[((i*j)/stride) % 4] << " x" << x[j] << " y" << y[i] << std::endl;
          }
       }
    }
@@ -38,42 +37,16 @@ std::cout << "i" << i << ",j" << j << ": w" << w[((i*j)/stride) % 4] << " x" << 
    {
       if (N % 2 == 0 && N > 2)
       {
-std::cout << "start x (N = " << N << ") = ";
-for (int ind = 0, count = 0; count < N; ind += stride, count++) {
-   std::cout << x[ind] << ", ";
-}
-std::cout << std::endl;
-
-std::cout << "FFT the evens (N = " << N << "): ";
-for (int ind = 0; ind < N/2; ind++) {
-   std::cout << x[2*stride*ind] << ", ";
-}
-std::cout << std::endl;
 
          // FFT the evens
          fftc2c (x, y, w, 2*stride, N/2);
-std::cout << "FFT (evens) (N = " << N << ") = ";
-for (int ind = 0; ind < N/2; ind++) {
-   std::cout << y[2*stride*ind] << ", ";
-}
-std::cout << std::endl;
 
          // Copy the result back into x for the evens
          for (int ind = 0, count = 0; count < N/2; ind += 2*stride, count++)
             x[ind] = y[ind];
 
-std::cout << "FFT the odds (N = " << N << "): ";
-for (int ind = 0; ind < N/2; ind++) {
-   std::cout << x[stride + 2*stride*ind] << ", ";
-}
-std::cout << std::endl;
          // FFT the odds
          fftc2c (x + stride, y + stride, w, 2*stride, N/2);
-std::cout << "FFT (odds) (N = " << N << ") = ";
-for (int ind = 0; ind < N/2; ind++) {
-   std::cout << y[stride + 2*stride*ind] << ", ";
-}
-std::cout << std::endl;
 
 //         // Copy the result back into x
 //         for (int ind = 0, count = 0; count < N; ind += stride, count++)
@@ -81,12 +54,6 @@ std::cout << std::endl;
          // Copy the result back into x for the odds
          for (int ind = stride, count = 0; count < N/2; ind += 2*stride, count++)
             x[ind] = y[ind];
-
-std::cout << "after x (N = " << N << ") = ";
-for (int ind = 0, count = 0; count < N; ind += stride, count++) {
-   std::cout << x[ind] << ", ";
-}
-std::cout << std::endl;
 
          // Apply the twiddle factors to the odd terms
          for (int ind = stride, wind = 0, count = 0;
@@ -96,33 +63,18 @@ std::cout << std::endl;
             x[ind] *= w[wind];
          }
 
-std::cout << "twiddle factors applied to x (N = " << N << ") = ";
-for (int ind = 0, count = 0; count < N; ind += stride, count++) {
-   std::cout << x[ind] << ", ";
-}
-std::cout << std::endl;
-
          for (int ind = 0, eind = 0, oind = stride, count = 0;
                count < N/2;
                ind += stride, eind += 2*stride, oind += 2*stride, count++)
          {
             y[ind             ] = x[eind] + x[oind];
             y[ind + N/2*stride] = x[eind] - x[oind];
-std::cout << "first half of y at N = 4: " << y[ind] << std::endl;
-std::cout << "last half of y at N = 4: " << y[ind + N/2] << std::endl;
          }
 
-std::cout << "fft (x) (N = " << N << ") = ";
-for (int ind = 0, count = 0; count < N; ind += stride, count++) {
-   std::cout << y[ind] << ", ";
-}
-std::cout << std::endl;
       }
       else {
-std::cout << "DFT of: " << x[0] << ", " << x[stride] << std::endl;
          // Compute the DFT
          dftc2c (x, y, w, stride, N);
-std::cout << "DFT = " << y[0] << ", " << y[stride] << std::endl;
       }
    }
 }
